a8dc0c84d81e2588f260bc061705eaa5fca225f6
sanket <wankhdes29@gmail.com>
2022-12-29 10:38:39 +0530

    able to make post request

--- script.js
+++ script.js
@@ -3,37 +3,59 @@
 
 let ourForm = document.getElementById("form");
 
-ourForm.addEventListener("submit", (event) => {
+ourForm.addEventListener("submit", async (event) => {
   event.preventDefault();
-
-  console.log("Hiii");
 
   let value = document.getElementById("textString").value;
 
   let encodedData = encodingFunction(value);
 
-  let data = encodedData;
+  let data = await encodedData;
 
-  console.log(data);
+  let data2 = await data.json();
+
+  // let data3 = data2.json();
+
+  let h1 = document.createElement("h2");
+
+  h1.textContent = data2.morseCode;
+
+  let encodingResult = document.getElementById("encodingResult");
+
+  encodingResult.append(h1);
 });
 
 let encodingFunction = async (value) => {
-  let url = "http://localhost:8888/encode";
+  // let url = `http://localhost:8888/encode/${value}`;
+
+  let url = `http://localhost:8888/encode`;
+
+  // let data = await fetch(url);
+
+  // let res = await data.json();
 
   let data = await fetch(url, {
     method: "POST",
 
     headers: {
-      Accept: "application/json",
       "Content-Type": "application/json",
     },
 
-    body: `{
-
-     "response":${value}
-
-    }`,
+    body: JSON.stringify(value),
   });
+
+  // return res;
 
   return data;
 };
+
+// const postRequest = async (newFood) => {
+//   let res = await fetch("http://localhost:3000/api/food", {
+//     method: "POST",
+//     body: JSON.stringify(newFood),
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//   });
+//   return res;
+// };
